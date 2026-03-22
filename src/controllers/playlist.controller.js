47e8b85defc29e88da3882a9bc8a1982a038447d
PatelJetun <jetunpatel54@gmail.com>
2026-03-22 14:15:48 +0530

    feat: add playlist creation and retrieval with user validation and error handling

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,16 +3,57 @@ import { Playlist } from "../models/playlist.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body;
-
+  const user = req?.user;
   //TODO: create playlist
+
+  if (!name || !description) {
+    throw new ApiError(400, "Playlist Name or Description Missing");
+  }
+
+  const playlist = await Playlist.create({
+    name,
+    description,
+    owner: user?._id,
+  });
+
+  if (!playlist) {
+    throw new ApiError(500, "An Error Occured While Creating Playlist");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, playlist, `Playlist: ${name}, Created Successfully`)
+    );
 });
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params;
   //TODO: get user playlists
+
+  if (!mongoose.isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid User Id");
+  }
+
+  if (!userId) {
+    throw new ApiError(400, "User Not Found");
+  }
+
+  const userPlaylist = await Playlist.find({ owner: userId });
+
+  if (!userPlaylist.length) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "No Playlist Exists For"));
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userPlaylist, "Playlist Fetched Successfully"));
 });
 
 const getPlaylistById = asyncHandler(async (req, res) => {
