0bf6d463abfa005afe9e04eb13cab17bbf8f1c8b
PatelJetun <jetunpatel54@gmail.com>
2026-04-02 16:27:06 +0530

    feat: implement toggleVideoLike function with validation and error handling

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,12 +1,48 @@
-import mongoose, { isValidObjectId } from "mongoose";
+import mongoose from "mongoose";
 import { Like } from "../models/like.model.js";
+import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import isValidId from "../utils/isValidId.js";
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: toggle like on video
+  const userId = req.user?._id;
+
+  if (!userId) {
+    throw new ApiError(401, "Unauthorized");
+  }
+
+  if (!isValidId(videoId)) {
+    throw new ApiError(400, "Invalid Video Id");
+  }
+
+  const videoExists = await Video.exists({ _id: videoId });
+
+  if (!videoExists) {
+    throw new ApiError(404, "Video does not exist");
+  }
+
+  const deletedLike = await Like.findOneAndDelete({
+    video: videoId,
+    likedBy: userId,
+  });
+
+  if (deletedLike) {
+    return res
+      .status(200)
+      .json(new ApiResponse(200, null, "Video unliked successfully"));
+  }
+
+  const newLike = await Like.create({
+    video: videoId,
+    likedBy: userId,
+  });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, newLike, "Video liked successfully"));
 });
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
